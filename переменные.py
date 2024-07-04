506c28554eedbfd5a731833fd3dbbbdfaa5eb202
ivan0uh <49480109+ivan0uh@users.noreply.github.com>
2024-07-04 19:35:02 +0300

    Add files via upload

---
+++ переменные.py
@@ -0,0 +1,9 @@
+completed_orders = 12
+hours_spent = 1.5
+Course_name = 'Python'
+time_one_task = completed_orders / hours_spent
+print ("Курс:" + str(Course_name), "всего задач:" + str(completed_orders), "затрачено часов:" + str(hours_spent), "среднее время выполнения " + str(time_one_task) + " часа.")
+
+
+
+
