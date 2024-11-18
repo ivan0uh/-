767981c6174a76cb00860ae368f2db2056dd9a16
ivan0uh <49480109+ivan0uh@users.noreply.github.com>
2024-11-19 01:31:40 +0300

    Add files via upload

--- dz.py
+++ dz.py
@@ -1,34 +1,35 @@
-calls = 0
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    if "@" not in recipient or not (
+        recipient.endswith(".com")
+        or recipient.endswith(".ru")
+        or recipient.endswith(".net")
+    ):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+    elif sender == recipient:
+        print("Нельзя отправить письмо самому себе!")
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    else:
+        print(
+            f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}."
+        )
 
 
-def count_cals(func):
-    def wrapper(*args, **kwargs):
-        global calls
-        calls += 1
-        result = func(*args, **kwargs)
-        return result
+send_email("Это сообщение для проверки связи", "vasyok1337@gmail.com")
 
-    return wrapper
+send_email(
+    "Вы видите это сообщение как лучший студент курса!",
+    "urban.fan@mail.ru",
+    sender="urban.info@gmail.com",
+)
+send_email(
+    "Пожалуйста, исправьте задание",
+    "urban.student@mail.ru",
+    sender="urban.teacher@mail.uk",
+)
 
-
-def string_info(input_string):
-    global calls
-    calls += 1
-    return (len(input_string), input_string.upper(), input_string.lower())
-
-
-def is_contains(string, list_to_search):
-    global calls
-    calls += 1
-    string_lower = string.lower()
-    for item in list_to_search:
-        if item.lower() == string_lower:
-            return True
-    return False
-
-
-print(string_info("Capybara"))
-print(string_info("Armageddon"))
-print(is_contains("Urban", ["ban", "BaNaN", "urBAN"]))  # Urban ~ urBAN
-print(is_contains("cycle", ["recycling", "cyclic"]))  # No matches
-print(calls)
+send_email(
+    "Напоминаю самому себе о вебинаре",
+    "urban.teacher@mail.ru",
+    sender="urban.teacher@mail.ru",
+)
